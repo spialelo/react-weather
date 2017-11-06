f41722ca4057b0f5a9d3ab8e24db321f05af8c90
spialelo <ndphillips@gmail.com>
2017-11-05 22:55:56 -0500

    Rendering each city query per row and added React sparklines to package

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+    constructor(props) {
+        super(props);
+        this.renderWeather = this.renderWeather.bind(this);
+    }
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        return (
+            <tr key={name}>
+                <td>{name}</td>
+            </tr>
+        );
+    }
     render () {
         return (
             <table className="table table-hover">
@@ -13,6 +25,9 @@ class WeatherList extends Component {
                         <th>Humidity</th>
                     </tr>    
                 </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
            </table>     
         );
     }
